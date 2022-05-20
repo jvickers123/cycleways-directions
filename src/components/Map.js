0d7b15386318a461de00ecfe8df8b16c468a3065
Jonny Vickers <jonathanbvickers@gmail.com>
2022-05-20 13:44:24 +0100

    installed semver and used create root for React 18

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React, {useState, useRef } from "react"
+
+// MAPBOX
+import ReactMapGl, { Source, Layer, Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from 'mapbox-gl'
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'
+mapboxgl.workerClass = MapboxWorker
+
 
 const Map = () => {
+
+  // STATE
+  const [viewPort, setViewPort] = useState({
+    latitude: 51.5,
+    longitude: -0.118,
+    zoom: 10
+  })
+
+  // REF
+  const mapRef = useRef()
+
   return (
-    <h1>Map</h1>
+    <div className='map-container'>
+      <ReactMapGl 
+          {...viewPort}
+          onMove={e => setViewPort(e.viewState)}
+          style={{ width: '100%', height: '100%' }}
+          mapStyle='mapbox://styles/jvickers/cl1kvl366000015qgczuks22r'
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          ref={mapRef}
+        >
+          </ReactMapGl>
+    </div>
   )
+    
 }
 
 export default Map
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import './styles/main.scss'
 import App from './App'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
     <App />
-);
+)
 
